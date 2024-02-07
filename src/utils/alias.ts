35a49e40e9d81477afbaac146e6db68390e9ec75
tjnoh <tjnoh@ciotsecurity.com>
2024-02-07 14:00:33 +0900

    240207 1400 [태종] merge 전

--- src/utils/alias.ts
+++ src/utils/alias.ts
@@ -1,21 +1,291 @@
 
-export const netWorkAlias: any = {
+export const networkAlias: any = {
     // alias    table명
-    id: "id", // 0
-    Accurancy: "정확도", // 1
-    Time: "탐지시각", // 2
-    PcName: "PC명", // 3
-    Agent_ip: "Agent Ip", // 4
-    SrcIp: "출발지 Ip", // 5
-    SrcPort: "출발지 Port", // 6
-    DstIp: "목적지 Ip", // 7
-    DstPort: "목적지 Port", // 8
-    Process: "프로세스명", // 9
-    PIDs: "PID", // 10
-    SrcFile: "유출 파일명", // 11
-    DownLoad: "파일 다운로드", // 12
-    ScreenShot: "스크린샷", // 13
-    FileSizes: "파일크기", // 14
-    Keywords: "탐지패턴", // 15
-    DestFiles: "URL", // 16
+    id: {              
+      name : "",     // 0
+      align : 'start',
+      width : '3%',
+    }, 
+    Accurancy: {       
+      name : "정확도", // 1
+      align : 'start',
+      width : '5%',
+    },
+    Time: {            
+      name : "탐지시각", // 2
+      align : 'start',
+      width : '8%',
+    },
+    PcName: {
+      name : "PC명",     // 3
+      align : 'start',
+      width : '8%',
+    },
+    Agent_ip: {
+      name : "Agent Ip", // 4 
+      align : 'start',
+      width : 'auto',
+    },
+    SrcIp: {
+      name : "출발지 Ip", // 5
+      align : 'start',
+      width : 'auto',
+    },
+    SrcPort: {
+      name : "출발지 Port", // 6
+      align : 'center',
+      width : '3%',
+    },
+    DstIp: {
+      name : "목적지 Ip", // 7
+      align : 'start',
+      width : 'auto',
+    },
+    DstPort: {
+      name : "목적지 Port", // 8
+      align : 'center',
+      width : '3%',
+    },
+    Process: {
+      name : "프로세스명", // 9
+      align : 'start',
+      width : 'auto',
+    },
+    PIDs: {
+      name : "PID", // 10
+      align : 'center',
+      width : '3%',
+    },
+    SrcFile: {
+      name : "유출 파일명", // 11
+      align : 'start',
+      width : 'auto',
+    },
+    DownLoad: {
+      name : "파일 다운로드", // 12
+      align : 'center',
+      width : '50px',
+    },
+    ScreenShot: {
+      name : "스크린샷", // 13
+      align : 'center',
+      width : '50px',
+    },
+    FileSizes: {
+      name : "파일크기", // 14
+      align : 'start',
+      width : 'auto',
+    },
+    Keywords: {
+      name : "탐지패턴", // 15
+      align : 'start',
+      width : 'auto',
+    },
+    DestFiles: {
+      name : "URL", // 16
+      align : 'start',
+      width : '8%',
+    },
+  };
+
+  export const mediaAlias:any = {
+    // alias    table명
+    'id' :  {
+      name : '',                   // 0
+      align : 'start',
+      width : 'auto',
+    },
+    'Time' : {
+      name : '탐지시각',           // 1
+      align : 'start',
+      width : 'auto',
+    },
+    'PcName' : {
+      name : 'PC명',           // 2
+      align : 'start',
+      width : 'auto',
+    },
+    'Agent_ip' : {
+      name : 'Agent Ip',       // 3
+      align : 'start',
+      width : 'auto',
+    },
+    'Process' : {
+      name : '프로세스명',         // 4
+      align : 'start',
+      width : 'auto',
+    },
+    'Media_Type' : {
+      name : '유출 유형',   // 5
+      align : 'start',
+      width : 'auto',
+    },
+    'Files' : {
+      name : '유출 파일명',              // 6
+      align : 'start',
+      width : 'auto',
+    },
+    'Copied_files' : {
+      name : '복사본', // 7
+      align : 'start',
+      width : 'auto',
+    },
+    'Downloading' : {
+      name : '파일 다운로드',  // 8
+      align : 'start',
+      width : 'auto',
+    },
+    'FileSizes' : {
+      name : '파일 크기',     // 9
+      align : 'start',
+      width : 'auto',
+    },
+    'Keywords' : {
+      name : '탐지패턴',       // 10
+      align : 'start',
+      width : 'auto',
+    },
+  };
+
+  export const outlookAlias:any = {
+    // alias    table명
+    'id' : {
+      name : '',                    // 0
+      align : 'start',
+      width : 'auto',
+    },
+    'Time' : {
+      name : '탐지시각',                // 1
+      align : 'start',
+      width : 'auto',
+    },
+    'PcName' : {
+      name : 'PC명',            // 2
+      align : 'start',
+      width : 'auto',
+    },
+    'Agent_ip' : {
+      name : 'Agent Ip',        // 3
+      align : 'start',
+      width : 'auto',
+    },
+    'Process' : {
+      name : '프로세스명',          // 4
+      align : 'start',
+      width : 'auto',
+    },
+    'PIDS' : {
+      name : 'pid',                 // 5
+      align : 'start',
+      width : 'auto',
+    },
+    'Mail_Subjects' : {
+      name : '메일명',    // 6
+      align : 'start',
+      width : 'auto',
+    },
+    'Sender' : {
+      name : '보낸사람',            // 7
+      align : 'start',
+      width : 'auto',
+    },
+    'Receiver' : {
+      name : '받은사람',        // 8
+      align : 'start',
+      width : 'auto',
+    },
+    'AttachedFiles' : {
+      name : '유출 파일명', // 9
+      align : 'start',
+      width : 'auto',
+    },
+    'CopiedFiles' : {
+      name : '복사본',   // 10
+      align : 'start',
+      width : 'auto',
+    },
+    'Downloading' : {
+      name : '파일 다운로드',   // 11
+      align : 'start',
+      width : 'auto',
+    },
+    'FileSizes' : {
+      name : '파일 크기',      // 12
+      align : 'start',
+      width : 'auto',
+    },
+    'Keywords' : {
+      name : '탐지 패턴',        // 13
+      align : 'start',
+      width : 'auto',
+    },
+  };
+
+  export const printAlias:any = {
+    // alias    table명
+    'id' : {
+      name : '',                       // 0
+      align : 'start',
+      width : 'auto',
+    },
+    'Time' : {
+      name : '탐지시각',                   // 1
+      align : 'start',
+      width : 'auto',
+    },
+    'PcName' : {
+      name : 'PC명',               // 2
+      align : 'start',
+      width : 'auto',
+    },
+    'Agent_ip' : {
+      name : 'Agent Ip',           // 3
+      align : 'start',
+      width : 'auto',
+    },
+    'Process' : {
+      name : '프로세스명',             // 4
+      align : 'start',
+      width : 'auto',
+    },
+    'PIDs' : {
+      name : 'pid',                    // 5
+      align : 'start',
+      width : 'auto',
+    },
+    'Printers' : {
+      name : '프린터',            // 6
+      align : 'start',
+      width : 'auto',
+    },
+    'Owners' : {
+      name : '관리자',                // 7
+      align : 'start',
+      width : 'auto',
+    },
+    'Documents' : {
+      name : '탐지문서',          // 8
+      align : 'start',
+      width : 'auto',
+    },
+    'Copied_Spool_Files' : {
+      name : '복사본', // 9
+      align : 'start',
+      width : 'auto',
+    },
+    'Downloading' : {
+      name : '파일 다운로드',        // 10
+      align : 'start',
+      width : 'auto',
+    },
+    'Sizes' : {
+      name : '복사본 크기',                  // 11
+      align : 'start',
+      width : 'auto',
+    },
+    'Pages' : {
+      name : '페이지',                 // 12
+      align : 'start',
+      width : 'auto',
+    },
   };
