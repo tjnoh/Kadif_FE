d3cb039273b16fb488bf432d8bbb075c21c04220
angwanghyeon <agh1594@naver.com>
2024-04-26 16:48:07 +0900

    240426 merge

--- src/utils/ipDomain.ts
+++ src/utils/ipDomain.ts
@@ -1,2 +1,2 @@
-export const frontIP = `https://172.31.168.112:3000`;
-export const backIP = `https://172.31.168.112:8000`;
+export const frontIP = `https://localhost:3000`;
+export const backIP = `https://localhost:8000`;
